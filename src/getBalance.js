373a28a6752c6371e703cfa5b01527a950d5c6ff
Kevin Klein <k.klein@intershop.de>
2021-03-29 23:53:32 +0200

    change getBalance.js

--- src/getBalance.js
+++ src/getBalance.js
@@ -4,8 +4,20 @@ const provider = require('./provider');
 
 async function main(){
   const account = wallet.connect(provider)
-  const balance = await account.getBalance();
-  console.log(`ETH Balance: ${ethers.utils.formatUnits(balance, 18)}`)
+
+  const usdc = new ethers.Contract(
+    "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
+    [
+      "function balanceOf(address _owner) public view returns (uint256 balance)",
+    ],
+    account
+  );
+
+  const ethBalance = await account.getBalance();
+  console.log(`ETH Balance: ${ethers.utils.formatEther(ethBalance, 18)}`)
+
+  const usdcBalance = await usdc.balanceOf(account.address)
+  console.log(`USDC Balance: ${ethers.utils.formatUnits(usdcBalance, 6)}` )
 }
 
 main()
